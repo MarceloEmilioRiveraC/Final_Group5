9113f31a6370506d4b67b3034dc5c2b7f25d03c5
Nohelis Rangel <rangelvick96@gmail.com>
2026-03-31 17:00:37 -0400

    Homepage.tsx and homepage.css was done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HomePage } from './presentation/pages/Home'
+// import { CataloguePage } from '@presentation/pages/Catalogue'
 
 function App() {
-
-
   return (
-   <></>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/catalogue" element={<CataloguePage />} /> */}
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- frontend/src/presentation/hooks/usePosts.ts
+++ frontend/src/presentation/hooks/usePosts.ts
@@ -1,20 +1,20 @@
-import { useEffect, useState } from "react";
-import { Post } from "../../domain/entities/Post";
-import { getPosts } from "../../application/posts/getPosts";
-import { PostRepository } from "../../infrastructure/repositories/PostRepository";
+// import { useEffect, useState } from "react";
+// import { Post } from "../../domain/entities/Post";
+// import { getPosts } from "../../application/posts/getPosts";
+// import { PostRepository } from "../../infrastructure/repositories/PostRepository";
 
-export const usePosts = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
+// export const usePosts = () => {
+//   const [posts, setPosts] = useState<Post[]>([]);
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const repo = new PostRepository();
-      const data = await getPosts(repo);
-      setPosts(data);
-    };
+//   useEffect(() => {
+//     const fetchPosts = async () => {
+//       const repo = new PostRepository();
+//       const data = await getPosts(repo);
+//       setPosts(data);
+//     };
 
-    fetchPosts();
-  }, []);
+//     fetchPosts();
+//   }, []);
 
-  return { posts };
-};
+//   return { posts };
+// };
